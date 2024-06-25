868ac72b770efc3fc4082542069731ef5f4e9a40
Roberto Martin <rmaguil@bancoagricola.com.sv>
2024-06-25 16:56:09 -0600

    Script BD

--- bd/script_creacion_bd.sql
+++ bd/script_creacion_bd.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS bd_garcia;
+CREATE DATABASE IF NOT EXISTS bd_Martin;
 
-USE bd_garcia;
+USE bd_Martin;
 
 CREATE TABLE IF NOT EXISTS person (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -10,3 +10,7 @@ CREATE TABLE IF NOT EXISTS person (
     puesto VARCHAR(255),
     sueldo DECIMAL(10, 2)
 );
+
+CREATE USER 'conexion'@'localhost' IDENTIFIED BY 'Pa$$word';
+GRANT ALL PRIVILEGES ON bd_Martin.* TO 'conexion'@'localhost';
+FLUSH PRIVILEGES;
